09390c72ae857a33910b2a4cb24acf44513a9477
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-06-29 01:34:45 +0300

    Change style to the css file again

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import s from './Components/Setting.module.css'
 import './Components/null.css';
 import {Settings} from './Components/Settings';
+import React from 'react';
 
 
 
@@ -8,6 +9,7 @@ function App() {
     return (
         <div className={s.app}>
             <div className={s.app__container}>
+                <div className={s.app__title}>This is my little React project.</div>
                 <Settings/>
             </div>
         </div>
